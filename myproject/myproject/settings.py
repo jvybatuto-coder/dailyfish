512e8529a0d31501572016d76eddb6122ec27b10
jvybatuto-coder <jvybatuto@gmail.com>
2025-11-23 00:11:10 +0800

    Switch to SQLite for deployment

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME', 'dailyfish'),
-        'USER': os.environ.get('DATABASE_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
-        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
-        'PORT': os.environ.get('DATABASE_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
