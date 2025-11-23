86ecca076485041634bf88578923e3eaf99304d9
jvybatuto-coder <jvybatuto@gmail.com>
2025-11-23 11:49:42 +0800

    Add temporary admin creation endpoint
    - Add create-admin/ endpoint to create admin user via web
    - This allows creating admin without shell access
    - Username: pelaez, Password: pelaez123

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from myapp.admin_utils import create_admin_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create-admin/', create_admin_user),
     path('', include('myapp.urls')),
 ]
 
