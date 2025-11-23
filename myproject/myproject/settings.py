9f850f025bb086457c33b0269aa062f1f004d814
jvybatuto-coder <jvybatuto@gmail.com>
2025-11-23 11:48:16 +0800

    Fix Django admin static files configuration
    - Add missing DEFAULT_AUTO_FIELD setting
    - Remove duplicate configuration lines
    - This should fix admin CSS/JS 404 errors on Render

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -125,6 +125,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -132,8 +137,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = 'fish_list'
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
